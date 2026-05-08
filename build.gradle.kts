cc12926972701e853948f6d96f8b84c57868d5de
vaniron <hrdlicka.luk@gmail.com>
2026-05-08 08:31:41 +0200

    Updated mysql, added mariadb driver, tweaked build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,6 @@ val targetJavaVersion = 21
 java {
     toolchain.languageVersion = JavaLanguageVersion.of(targetJavaVersion)
     // Loom will automatically attach sourcesJar to a RemapSourcesJar task and to the "build" task
-    // if it is present.
-    // If you remove this line, sources will not be generated.
     withSourcesJar()
 }
 
@@ -35,11 +33,6 @@ loom {
 }
 
 repositories {
-    // Add repositories to retrieve artifacts from in here.
-    // You should only use this when depending on other mods because
-    // Loom adds the essential maven repositories to download Minecraft and libraries from automatically.
-    // See https://docs.gradle.org/current/userguide/declaring_repositories.html
-    // for more information about repositories.
     // China-friendly mirror for Maven Central and common public artifacts.
     maven {
         name = "Aliyun Public"
@@ -99,48 +92,41 @@ repositories {
 }
 
 dependencies {
-    // To change the versions see the gradle.properties file
+    // Environment setup
     minecraft("com.mojang:minecraft:${project.property("minecraft_version")}")
     mappings("net.fabricmc:yarn:${project.property("yarn_mappings")}:v2")
     modImplementation("net.fabricmc:fabric-loader:${project.property("loader_version")}")
     modImplementation("net.fabricmc:fabric-language-kotlin:${project.property("kotlin_loader_version")}")
-    modImplementation("net.fabricmc.fabric-api:fabric-networking-api-v1:${project.property("fabric_version")}")
 
+    // Fabric API
     modImplementation("net.fabricmc.fabric-api:fabric-api:${project.property("fabric_version")}")
 
-    // PlaceholderAPI
+    // External Mods
     modImplementation("eu.pb4:placeholder-api:${project.property("placeholder_api_version")}")
-
-    // Cobblemon
     modImplementation("com.cobblemon:fabric:${project.property("cobblemon_version")}")
 
-    // MySQL 驱动
-    implementation("mysql:mysql-connector-java:8.0.33")
-
-    // Database
-    implementation("org.xerial:sqlite-jdbc:3.45.1.0")
-    include("org.xerial:sqlite-jdbc:3.45.1.0")
-
-    // Configuration
-    implementation("blue.endless:jankson:1.2.3")
-    include("blue.endless:jankson:1.2.3")
-
-    // Exposed ORM
-    implementation("org.jetbrains.exposed:exposed-core:0.49.0")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.49.0")
-    include("org.jetbrains.exposed:exposed-core:0.49.0")
-    include("org.jetbrains.exposed:exposed-jdbc:0.49.0")
-
-    implementation("com.squareup.okhttp3:okhttp:4.12.0") // For WebSocket
-    include("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("com.squareup.okio:okio:3.9.0") // For WebSocket
-    include("com.squareup.okio:okio:3.9.0")
+    // --- Bundled Libraries (Jar-in-Jar) ---
+    // Uses a loop to safely apply both implementation() and include() without Kotlin DSL type errors
+    listOf(
+        "com.mysql:mysql-connector-j:8.4.0",           // MySQL
+        "org.mariadb.jdbc:mariadb-java-client:3.5.8",  // MariaDB
+        "org.xerial:sqlite-jdbc:3.45.1.0",             // SQLite
+        "blue.endless:jankson:1.2.3",                  // Configuration
+        "org.jetbrains.exposed:exposed-core:0.49.0",   // Exposed ORM
+        "org.jetbrains.exposed:exposed-jdbc:0.49.0",   // Exposed JDBC
+        "com.squareup.okhttp3:okhttp:4.12.0",          // OkHttp
+        "com.squareup.okio:okio:3.9.0"                 // Okio
+    ).forEach { lib ->
+        implementation(lib)
+        include(lib)
+    }
 }
 
 tasks.processResources {
     inputs.property("version", project.version)
     inputs.property("minecraft_version", project.property("minecraft_version"))
     inputs.property("loader_version", project.property("loader_version"))
+    inputs.property("kotlin_loader_version", project.property("kotlin_loader_version"))
     filteringCharset = "UTF-8"
 
     filesMatching("fabric.mod.json") {
@@ -154,25 +140,21 @@ tasks.processResources {
 }
 
 tasks.withType<JavaCompile>().configureEach {
-    // ensure that the encoding is set to UTF-8, no matter what the system default is
-    // this fixes some edge cases with special characters not displaying correctly
-    // see http://yodaconditions.net/blog/fix-for-java-file-encoding-problems-with-gradle.html
-    // If Javadoc is generated, this must be specified in that task too.
     options.encoding = "UTF-8"
     options.release.set(targetJavaVersion)
 }
 
 tasks.withType<KotlinCompile>().configureEach {
-    compilerOptions.jvmTarget.set(JvmTarget.fromTarget(targetJavaVersion.toString()))
+    compilerOptions.jvmTarget.set(JvmTarget.JVM_21)
 }
 
 tasks.jar {
     from("LICENSE") {
-        rename { "${it}_${project.base.archivesName}" }
+        // Added .get() to archivesName to safely unwrap the property provider
+        rename { "${it}_${project.base.archivesName.get()}" }
     }
 }
 
-// configure the maven publication
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
@@ -180,12 +162,7 @@ publishing {
             from(components["java"])
         }
     }
-
-    // See https://docs.gradle.org/current/userguide/publishing_maven.html for information on how to set up publishing.
     repositories {
-        // Add repositories to publish to here.
-        // Notice: This block does NOT have the same function as the block in the top level.
-        // The repositories here will be used for publishing your artifact, not for
-        // retrieving dependencies.
+        // Publishing repositories setup goes here
     }
 }
